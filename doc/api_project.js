764b0c2d943842a499905e37634e7571eb72deeb
ar <ar@narola.email>
2017-12-06 14:51:18 +0530

    document changes

--- doc/api_project.js
+++ doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-12-06T04:56:17.471Z",
+    "time": "2017-12-06T09:21:08.623Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
