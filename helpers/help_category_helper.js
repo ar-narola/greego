88fe711bc9d7b401759522ea2140e8f146c87015
ar <ar@narola.email>
2018-01-19 12:34:30 +0530

    Listing of category, update category

--- helpers/help_category_helper.js
+++ helpers/help_category_helper.js
@@ -27,4 +27,69 @@ category_helper.insert_category = function(category_object,callback){
     });
 };
 
+/*
+ * update_category_by_id is used to update category in database
+ * 
+ * @param   category_id String  _id of user that need to be update
+ * @param   update_obj  JSON object consist of all property that need to update in collection
+ * 
+ * @return  status  0 - If any error occur in updating category, with error
+ *          status  1 - If category updated successfully, with appropriate message
+ *          status  2 - If category not updated, with appropriate message
+ * 
+ * @developed by "ar"
+ */
+category_helper.update_category_by_id = function(category_id,update_obj,callback){
+    HelpCategory.update({_id:{$eq : category_id}},{$set:update_obj},function(err,update_data){
+        if (err) {
+            callback({"status":0,"err":err});
+        } else {
+            if (update_data.nModified == 1) {
+                callback({"status":1,"message":"Record has been updated"});
+            } else {
+                callback({"status":2,"message":"Record has not updated"});
+            }
+        }
+    });
+};
+
+/*
+ * delete_category_by_id is used to delete category in database
+ * 
+ * @param   category_id String  _id of user that need to be delete
+ * 
+ * @return  status  0 - If any error occur in deletion of category, with error
+ *          status  1 - If category deleted successfully, with appropriate message
+ * 
+ * @developed by "ar"
+ */
+category_helper.delete_category_by_id = function(category_id,callback){
+    HelpCategory.remove({_id:{$eq : category_id}},function(err,delete_resp){
+        if (err) {
+            callback({"status":0,"err":err});
+        } else {
+            callback({"status":1,"message":"Record has been deleted"});
+        }
+    });
+};
+
+category_helper.get_all_category = function(callback){
+    HelpCategory.aggregate([
+        {
+            "$lookup":{
+                "from":"help_categories",
+                "localField":"parent_id",
+                "foreignField":"_id",
+                "as":"parent"
+            }
+        }
+    ],function(err,category){
+        if(err){
+            callback(err);
+        } else {
+            callback(category);
+        }
+    });
+}
+
 module.exports = category_helper;
--- routes/admin/category.js
+++ routes/admin/category.js
@@ -13,6 +13,29 @@ var logger = config.logger;
 var category_helper = require('../../helpers/help_category_helper');
 
 /**
+ * @api {post} /category Get all category
+ * @apiName Get all category
+ * @apiGroup Admin
+ * 
+ * @apiHeader {String}  x-access-token Admin's unique access-key
+ * 
+ * @apiParam {String} name Name of category
+ * @apiParam {String} [parent_id] Parent category Id
+ * @apiParam {String} [content] Additional description for category
+ * @apiParam {File} [icon] Icon image of user
+ * 
+ * @apiDescription  You need to pass form-data
+ * 
+ * @apiSuccess (Success 200) {JSON} driver Driver details
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+router.get('/',function(req,res){
+    category_helper.get_all_category(function(category_data){
+        res.status(config.OK_STATUS).json(category_data);
+    });
+});
+
+/**
  * @api {post} /category/create Create category
  * @apiName Create category
  * @apiGroup Admin
@@ -24,7 +47,7 @@ var category_helper = require('../../helpers/help_category_helper');
  * @apiParam {String} [content] Additional description for category
  * @apiParam {File} [icon] Icon image of user
  * 
-* @apiDescription  You need to pass form-data
+ * @apiDescription  You need to pass form-data
  * 
  * @apiSuccess (Success 200) {JSON} driver Driver details
  * @apiError (Error 4xx) {String} message Validation or error message.
@@ -89,6 +112,113 @@ router.post('/create',function(req, res){
                             res.status(config.INTERNAL_SERVER_ERROR).json({"error":resp.err});
                         } else {
                             res.status(config.OK_STATUS).json({"message":"Category inserted successfully","category":resp.category});
+                        }
+                    });
+                }
+            ], function (err, result) {
+                logger.trace("execution finished");
+                if (err) {
+                    if(err.status == config.VALIDATION_FAILURE_STATUS){
+                        res.status(err.status).json({"message": err.err,"error":err.error});
+                    } else {
+                        res.status(err.status).json({"message": err.err});
+                    }
+                } else {
+                    res.status(config.OK_STATUS).json({"message": "Profile information has been updated successfully","user":result});
+                }
+            });
+        } else {
+            var result = {
+                message: "Validation Error",
+                error: result.array()
+            };
+            res.status(config.VALIDATION_FAILURE_STATUS).json(result);
+        }
+    });
+});
+
+/**
+ * @api {post} /category/update Update category
+ * @apiName Update category
+ * @apiGroup Admin
+ * 
+ * @apiHeader {String}  x-access-token Admin's unique access-key
+ * 
+ * @apiParam {String} id Category Id
+ * @apiParam {String} name Name of category
+ * @apiParam {String} [parent_id] Parent category Id
+ * @apiParam {String} [content] Additional description for category
+ * @apiParam {File} [icon] Icon image of user
+ * 
+* @apiDescription  You need to pass form-data
+ * 
+ * @apiSuccess (Success 200) {JSON} driver Driver details
+ * @apiError (Error 4xx) {String} message Validation or error message.
+ */
+router.post('/update',function(req, res){
+    var schema = {
+        "id":{
+            notEmpty: true,
+            errorMessage: "Category id is required"
+        }
+    };
+
+    req.checkBody(schema);
+    req.getValidationResult().then(function (result) {
+        if (result.isEmpty()) {
+            async.waterfall([
+                function (callback) {
+                    // Upload user avatar
+                    logger.trace("Uploading category icon in create category API");
+                    if (req.files && req.files['icon']) {
+                        var file = req.files['icon'];
+                        var dir = "./uploads/category_icon";
+                        var mimetype = ['image/png', 'image/jpeg', 'image/jpg'];
+
+                        if (mimetype.indexOf(file.mimetype) != -1) {
+                            if (!fs.existsSync(dir)) {
+                                fs.mkdirSync(dir);
+                            }
+                            extention = path.extname(file.name);
+                            filename = "category_" + new Date().getTime() + extention;
+                            file.mv(dir + '/' + filename, function (err) {
+                                if (err) {
+                                    logger.error("There was an issue in uploading image");
+                                    callback({"status": config.MEDIA_ERROR_STATUS, "err": "There was an issue in uploading category icon"});
+                                } else {
+                                    logger.trace("Category icon has been uploaded. Image name = ",filename);
+                                    callback(null, filename);
+                                }
+                            });
+                        } else {
+                            logger.error("Image format is invalid");
+                            callback({"status": config.VALIDATION_FAILURE_STATUS, "err": "Image format is invalid"});
+                        }
+                    } else {
+                        logger.info("Image not available to upload. Executing next instruction");
+                        callback(null, null);
+                    }
+                },
+                function(image_name,callback){
+                    var obj = {};
+                    if(req.body.name){
+                        obj.name = req.body.name;
+                    }
+                    
+                    if(req.body.parent_id){
+                        obj.parent_id = req.body.parent_id;
+                    }
+                    if(req.body.content){
+                        obj.content = req.body.content;
+                    }
+                    if(image_name && image_name != null){
+                        obj.image = image_name;
+                    }
+                    category_helper.update_category_by_id(req.body.id,obj,function(resp){
+                        if(resp.status == 0){
+                            res.status(config.INTERNAL_SERVER_ERROR).json({"error":resp.err});
+                        } else {
+                            res.status(config.OK_STATUS).json({"message":"Category has been updated successfully"});
                         }
                     });
                 }
