f21dc90b7b8ef8d25bdeeb7821f523a6c35f9988
ar <ar@narola.email>
2018-01-25 18:04:39 +0530

    no message

--- helpers/invoice_helper.js
+++ helpers/invoice_helper.js
@@ -1,4 +1,6 @@
 var async = require('async');
+var _ = require('underscore');
+var moment = require('moment');
 
 var Invoice = require("../models/invoice");
 var invoice_helper = {};
@@ -12,6 +14,12 @@ invoice_helper.get_paid_invoice_for_driver = function (driver_id, callback) {
             callback({"status": 0, "err": err});
         } else {
             if (invoice_data.length > 0) {
+                
+                _.map(invoice_data, function(invoice){ 
+                    invoice.start_date = moment(invoice.start_date).format('MMMM Do YYYY, h:mm:ss a');
+                    return invoice; 
+                });
+                
                 callback({"status": 1, "invoice": invoice_data});
             } else {
                 callback({"status": 404, "err": "No invoice found"});
